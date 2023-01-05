81f0c6f266b2dbd9e496e1587f7a574792916140
PetrDedic <petandedic@gmail.com>
2023-01-05 14:18:20 +0100

    Basic filter

--- erescom/components/Accordion.jsx
+++ erescom/components/Accordion.jsx
@@ -1,28 +1,37 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 const StyledAccordion = styled.div`
   height: auto;
-  width: 100%;
+  width: calc(100% - 6rem);
 
   display: flex;
   flex-direction: column;
 
-  margin: 1rem 0;
+  margin: 1rem auto;
 
   .outer {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
 
+    padding: 1rem;
+    border-radius: 1rem;
+
+    background-color: #1b95f316;
+
     cursor: pointer;
 
-    border-bottom: solid lightgray 1px;
+    &.true {
+      border-radius: 1rem 1rem 0 0;
+
+      background-color: #1b95f340;
+    }
 
     .icon {
       align-self: center;
@@ -37,6 +46,13 @@ const StyledAccordion = styled.div`
     li {
       margin: 0.5rem 0;
     }
+
+    &.true {
+      padding: 1rem;
+      border-radius: 0 0 1rem 1rem;
+
+      background-color: #1b95f316;
+    }
   }
 `;
 
@@ -44,16 +60,19 @@ const Accordion = (props) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <StyledAccordion className="icon" open={open}>
-      <div className="outer" onClick={() => setOpen(!open)}>
+    <StyledAccordion className={"icon " + open} open={open}>
+      <div className={"outer " + open} onClick={() => setOpen(!open)}>
         <h3>{props.header}</h3>
         {!open ? (
-          <FontAwesomeIcon icon={faPlus} />
+          <FontAwesomeIcon
+            icon={faChevronDown}
+            style={{ alignSelf: "center" }}
+          />
         ) : (
-          <FontAwesomeIcon icon={faMinus} />
+          <FontAwesomeIcon icon={faChevronUp} style={{ alignSelf: "center" }} />
         )}
       </div>
-      <div className="inner">
+      <div className={"inner " + open}>
         <p>{props.text}</p>
       </div>
     </StyledAccordion>
